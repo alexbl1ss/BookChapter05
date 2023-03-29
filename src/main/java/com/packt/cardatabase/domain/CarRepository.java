0bbebe91fff821eea792d107c0c834380bb5a938
alexbl1ss <alex@brownleeschools.com>
2023-03-29 15:39:27 +0100

    new users added and stored proc tpoc

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,6 +2,7 @@ package com.packt.cardatabase.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,4 +14,13 @@ public interface CarRepository extends CrudRepository<Car, Long>  {
 
 	// Fetch cars by color
 	List<Car> findByColor(@Param("color") String color);
+
+	// Fetch cars by year
+	List<Car> findByYear(@Param("year") int year);
+
+	// Fetch cars by year
+	List<Car> findByBrandAndYear(@Param("brand") String brand, @Param("year") int year);
+
+    @Query(value = "CALL simpleCar;", nativeQuery = true)
+    List<Car> simpleCar();
 }
